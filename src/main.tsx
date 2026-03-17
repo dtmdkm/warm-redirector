7ceb985c0fb3741086a5152e341b89f6585288e0
dtmdkm <tuanminhlionmedia@gmail.com>
2026-03-17 21:41:02 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
+import { Analytics } from '@vercel/analytics/react';
+import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>
+);
